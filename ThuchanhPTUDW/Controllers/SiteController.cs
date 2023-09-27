934e723993f7a4b44ab155dc97c1f16ebe7491da
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2023-09-27 19:43:11 +0700

    Init

--- ThuchanhPTUDW/Controllers/SiteController.cs
+++ ThuchanhPTUDW/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace ThuchanhPTUDW.Controllers
 {
@@ -11,6 +12,9 @@ namespace ThuchanhPTUDW.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();//tao moi database
+            int sodong = db.Products.Count();
+            ViewBag.sodong = sodong;
             return View();
         }
     }

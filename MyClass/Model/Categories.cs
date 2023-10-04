1f2a4dea3c591aff6bae881530b82b3d9ac6d3c7
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2023-10-04 20:14:34 +0700

    init

--- MyClass/Model/Categories.cs
+++ MyClass/Model/Categories.cs
@@ -13,28 +13,50 @@ namespace MyClass.Model
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Không được để trống")]
+        [Display(Name = "Tên loại hàng")]
         public string Name { get; set; }
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
+
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
+
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
-        [Required]
-        public string MetaDesc  { get; set; }
-        [Required]
+
+        [Display(Name = "Mô tả")]
+        [Required (ErrorMessage ="Không được để trống")]
+        public string MetaDesc { get; set; }
+
+        [Display(Name = "Từ khóa")]
+        [Required(ErrorMessage = "Không được để trống")]
         public string MetaKey { get; set; }
-        [Required]
+
+        [Display(Name = "Tên rút gọn")]
+        [Required(ErrorMessage = "Không được để trống")]
         public int CreateBy { get; set; }
+
+        [Display(Name = "Tên rút gọn")]
         [Required]
         public DateTime CreateAt { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Không được để trống")]
+        [Display(Name = "Người cập nhật")]
+        
         public int UpdateBy { get; set; }
+
+        [Display(Name = "Tên rút gọn")]
         [Required]
         public DateTime UpdateAt { get; set; }
+
+        [Display(Name = "Trạng thái")]
         [Required]
         public int Status { get; set; }
 
+       
 
 
 
+    
     }
 }
